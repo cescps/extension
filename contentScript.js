cafe1eb507b890acc84d6f991ddede6962c866fc
Cesc <83516572+cescps@users.noreply.github.com>
2023-05-25 16:37:59 +0200

    Create contentScript.js

---
+++ contentScript.js
@@ -0,0 +1,15 @@
+// contentScript.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'applyFont') {
+    document.body.style.fontFamily = request.font;
+  }
+  else if (request.action === 'applyColor') {
+    document.body.style.color = request.color;
+  }
+  else if (request.action === 'deleteElements') {
+    var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].remove();
+    }
+  }
+});
