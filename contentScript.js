eb2fdde279210be42b29fb289db1ef77761a98a5
Cesc <83516572+cescps@users.noreply.github.com>
2023-05-26 10:40:53 +0200

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,48 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('Received message:', request);
+// This code will change the font and color of the text on the page.
 
-// contentScript.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'applyFont') {
-    document.body.style.fontFamily = request.font;
-  }
-  else if (request.action === 'applyColor') {
-    document.body.style.color = request.color;
-  }
-  else if (request.action === 'deleteElements') {
-    var buttons = document.getElementsByTagName('button');
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].remove();
-    }
-  }
+var fonts = ["Arial", "Times New Roman", "Verdana"];
+var colors = ["black", "red", "blue"];
+
+var currentFont = fonts[0];
+var currentColor = colors[0];
+
+document.querySelectorAll("body").forEach(function(element) {
+  element.style.fontFamily = currentFont;
+  element.style.color = currentColor;
 });
+
+// This function will be called when the user clicks on the extension icon.
+
+function changeFont() {
+  var newFont = prompt("Select a font:", currentFont);
+  if (newFont) {
+    currentFont = newFont;
+    updateFonts();
+  }
+}
+
+// This function will be called when the user clicks on the extension icon.
+
+function changeColor() {
+  var newColor = prompt("Select a color:", currentColor);
+  if (newColor) {
+    currentColor = newColor;
+    updateColors();
+  }
+}
+
+// This function will update the fonts and colors on the page.
+
+function updateFonts() {
+  document.querySelectorAll("body").forEach(function(element) {
+    element.style.fontFamily = currentFont;
+  });
+}
+
+// This function will update the colors on the page.
+
+function updateColors() {
+  document.querySelectorAll("body").forEach(function(element) {
+    element.style.color = currentColor;
+  });
+}
